f10655df704d1009fa965ee90318138c6cb6ce5a
SyedFazal110 <syedfazal808@gmail.com>
2024-06-28 10:44:11 +0500

    ts file created

---
+++ Class-1/app.ts
@@ -0,0 +1,21 @@
+// let car:string = "BMW"
+
+// type data = {
+//     firstName : string;
+//     age : number;
+//     email : string;
+// }
+// type data = {
+//     lastname : string
+// }
+
+// let form:data = {
+//     firstName : "Fazal",
+//     age : 22,
+//     email : "fazalali@gmail.com"
+// }
+
+
+
+
+type data = (string | number)[]
